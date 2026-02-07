6875d0649635ab33a0f62b1aaf2f227ae9a99855
TypJakob <87580940+TypJakob@users.noreply.github.com>
2026-02-07 20:26:08 +0100

    feat: adjust attackstates timing

--- Assets/Scripts/Player/StateMachine/Helpers/AttackComboDefinition.cs
+++ Assets/Scripts/Player/StateMachine/Helpers/AttackComboDefinition.cs
@@ -12,45 +12,45 @@ namespace Player.StateMachine
                 AnimationStateName = "Stand_Attack_01 1",
                 StartPose = AttackPoseDirection.RightUp,
                 EndPose = AttackPoseDirection.LeftDown,
-                SlashStartTime = 0.1f,
-                RecoveryStartTime = 0.3f,
-                ComboWindowStart = 0.3f,
+                SlashStartTime = 0.3f,
+                RecoveryStartTime = 0.4f,
+                ComboWindowStart = 0.4f,
                 ExitTime = 0.95f
             },
             new() {
                 AnimationStateName = "Stand_Attack_03 2",
                 StartPose = AttackPoseDirection.LeftDown,
                 EndPose = AttackPoseDirection.RightUp,
-                SlashStartTime = 0.1f,
-                RecoveryStartTime = 0.3f,
-                ComboWindowStart = 0.3f,
+                SlashStartTime = 0.3f,
+                RecoveryStartTime = 0.4f,
+                ComboWindowStart = 0.4f,
                 ExitTime = 0.95f
             },
             new() {
                 AnimationStateName = "Stand_Attack_01 4",
                 StartPose = AttackPoseDirection.RightUp,
                 EndPose = AttackPoseDirection.RightDown,
-                SlashStartTime = 0.2f,
-                RecoveryStartTime = 0.4f,
-                ComboWindowStart = 0.4f,
+                SlashStartTime = 0.4f,
+                RecoveryStartTime = 0.5f,
+                ComboWindowStart = 0.5f,
                 ExitTime = 0.95f
             },
             new() {
                 AnimationStateName = "LightAttack01",
                 StartPose = AttackPoseDirection.RightDown,
                 EndPose = AttackPoseDirection.LeftUp,
-                SlashStartTime = 0.1f,
-                RecoveryStartTime = 0.3f,
-                ComboWindowStart = 0.3f,
+                SlashStartTime = 0.3f,
+                RecoveryStartTime = 0.4f,
+                ComboWindowStart = 0.4f,
                 ExitTime = 0.95f
             },
             new() {
                 AnimationStateName = "LightAttack06",
                 StartPose = AttackPoseDirection.LeftUp,
                 EndPose = AttackPoseDirection.LeftDown,
-                SlashStartTime = 0.2f,
-                RecoveryStartTime = 0.4f,
-                ComboWindowStart = 0.4f,
+                SlashStartTime = 0.4f,
+                RecoveryStartTime = 0.5f,
+                ComboWindowStart = 0.5f,
                 ExitTime = 0.95f
             }
         };
