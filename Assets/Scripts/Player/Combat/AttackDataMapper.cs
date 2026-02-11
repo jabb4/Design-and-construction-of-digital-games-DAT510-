0bf0b55d1c9ae70f28a2cd3df31b23e2ec56217a
TypJakob <87580940+TypJakob@users.noreply.github.com>
2026-02-11 23:21:44 +0100

    refactor: replcae player attack step to shared attack data

---
+++ Assets/Scripts/Player/Combat/AttackDataMapper.cs
@@ -0,0 +1,17 @@
+using Combat;
+using Player.StateMachine;
+
+namespace Player.Combat
+{
+    public static class AttackDataMapper
+    {
+        public static AttackData ToAttackData(AttackStep step)
+        {
+            return new AttackData
+            {
+                AttackId = step.AnimationStateName,
+                Damage = step.Damage
+            };
+        }
+    }
+}
