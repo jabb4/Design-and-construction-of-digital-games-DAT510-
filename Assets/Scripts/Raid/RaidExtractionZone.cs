2055df8fb0a572c89e2581770361f8c4958b007b
Jacob <87302129+jabb4@users.noreply.github.com>
2026-02-24 13:09:50 +0100

    Created system for raid extraction
    Implementad raid extraction features:
    - An extraction zone where you are able to extract from, this as a dop in prefab
    - Extraction UI that tells you to hold down a key to extract and how long untill you extract. This is also as a drop in prefab, should be put in under canvas object.
    
    > Make sure to tag the player obejct with the Player tag, else the colider wont work in the extraction zone.

---
+++ Assets/Scripts/Raid/RaidExtractionZone.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class RaidExtractionZone : MonoBehaviour
+{
+    [SerializeField] private float holdTime = 10f;
+    [SerializeField] private int targetSceneIndex = 2;
+    [SerializeField] private Key activationKey = Key.E;
+    [SerializeField] private RaidExtractionUI extractionUI;
+    [SerializeField] private AudioClip extractionSound;
+    [SerializeField] private AudioSource audioSource;
+
+
+    private bool isPlayerInZone = false;
+    private bool isExtracting = false;
+    private float currentHoldTime = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isExtracting) return;
+
+        if (isPlayerInZone)
+        {
+            if (Keyboard.current != null && Keyboard.current[activationKey].isPressed)
+            {
+                currentHoldTime += Time.deltaTime;
+
+                float remainingTime = Mathf.Max(0, holdTime - currentHoldTime);
+                if (extractionUI != null) extractionUI.UpdateCountdown(remainingTime);
+
+                if (currentHoldTime >= holdTime)
+                {
+                    if (audioSource != null && extractionSound != null)
+                    {
+                        audioSource.PlayOneShot(extractionSound);
+                    }
+                    StartCoroutine(DisplayFadeAndExitRaid());
+                }
+            }
+            else
+            {
+                currentHoldTime = 0f;
+                if (extractionUI != null) extractionUI.ResetText();
+            }
+        }
+        else
+        {
+            currentHoldTime = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInZone = true;
+            if (extractionUI != null) extractionUI.Show();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInZone = false;
+            currentHoldTime = 0f;
+            if (extractionUI != null) extractionUI.Hide();
+        }
+    }
+
+    private IEnumerator DisplayFadeAndExitRaid()
+    {
+        isExtracting = true;
+
+        if (extractionUI != null)
+        {
+            yield return extractionUI.FadeToBlack();
+        }
+
+        SaveManager.Instance.SaveGameData();
+        SceneManager.LoadScene(targetSceneIndex);
+    }
+}
--- credits.md
+++ credits.md
@@ -14,6 +14,7 @@
 - Map opening - [monosfera](https://freesound.org/people/monosfera/sounds/490681/)
 - Wind in trees - [nicotep](https://freesound.org/people/nicotep/sounds/843926/)
 - Car motor - [enric592](https://freesound.org/people/enric592/sounds/185743/)
+- Car starting - [giddster](https://freesound.org/people/giddster/sounds/437352/)
 
 ### Music
 - Main menu music - [Kike Gutz](https://www.freepik.com/audio/tune/taiko-flame-call)
