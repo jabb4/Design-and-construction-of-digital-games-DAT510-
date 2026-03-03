df2888e72295a9924ee111ab347439a18a89ac18
Jacob <87302129+jabb4@users.noreply.github.com>
2026-03-03 16:52:18 +0100

    Implemented fuel system into raid map

--- Assets/Scripts/VanView/RaidMap/MapLocations.cs
+++ Assets/Scripts/VanView/RaidMap/MapLocations.cs
@@ -1,51 +1,107 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using TMPro;
 
 public class MapLocations : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
 {
-    private Image image;
-    public Color hoverColor = new Color32(0, 0, 0, 100);
-    private Color transparentColor = new Color32(0, 0, 0, 0);
+    public enum Destination { Van, City, Temple }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private TMP_Text titleText;
+    [SerializeField] private Destination destination;
+    [SerializeField] private float travelCost = 0f;
+    [SerializeField] private float travelDistance = 0f;
+    [SerializeField] private Color affordHoverColor = Color.green;
+    [SerializeField] private Color cantAffordHoverColor = Color.red;
+    [SerializeField] private float hoverScale = 1.1f;
+    [SerializeField] private float transitionSpeed = 20f;
+
+    private Vector3 originalScale;
+    private Vector3 targetScale;
+    private Color originalColor;
+
+    void Awake()
     {
-        image = GetComponent<Image>();
-        image.color = transparentColor;
+        originalScale = titleText.transform.localScale;
+        originalColor = titleText.color;
+    }
+
+    void OnEnable()
+    {
+        if (destination == Destination.Van)
+        {
+            titleText.text = destination.ToString();
+        }
+        else
+        {
+            titleText.text = $"{destination} - {travelDistance}km\n Cost: {travelCost}l";
+        }
+
+        if (titleText != null)
+        {
+            titleText.transform.localScale = originalScale;
+            targetScale = originalScale;
+            titleText.color = originalColor;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (titleText == null) return;
 
+        if (titleText.transform.localScale != targetScale)
+        {
+            titleText.transform.localScale = Vector3.Lerp(titleText.transform.localScale, targetScale, Time.deltaTime * transitionSpeed);
+
+            if (Vector3.Distance(titleText.transform.localScale, targetScale) < 0.001f)
+                titleText.transform.localScale = targetScale;
+        }
     }
     public void OnPointerClick(PointerEventData eventData)
     {
-        switch (tag)
+        bool canAffordTravel = GameStateManager.Instance.GetFuelAmount() >= travelCost;
+
+        if (!canAffordTravel)
         {
-            case "van":
+            return;
+        }
+        else if (destination != Destination.Van)
+        {
+            GameStateManager.Instance.RemoveFuel((int)travelCost);
+            GameStateManager.Instance.SaveGameState();
+        }
+
+        switch (destination)
+        {
+            case Destination.Van:
                 Debug.Log("Going back to VAN");
                 RaidMapUIController.Instance.GoToVan();
                 break;
-            case "city":
+            case Destination.City:
                 Debug.Log("Entering CITY");
                 RaidMapUIController.Instance.GoToCity();
                 break;
-            case "temple":
+            case Destination.Temple:
                 Debug.Log("Entering TEMPLE");
                 RaidMapUIController.Instance.GoToTemple();
                 break;
             default:
-                // no action for other tags
                 break;
         }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        image.color = hoverColor;
+        Color hoverColor = GameStateManager.Instance.GetFuelAmount() >= travelCost
+            ? affordHoverColor
+            : cantAffordHoverColor;
 
+        if (titleText != null)
+        {
+            targetScale = originalScale * hoverScale;
+            titleText.color = hoverColor;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -55,6 +111,10 @@ public class MapLocations : MonoBehaviour, IPointerEnterHandler, IPointerClickHa
 
     public void ResetHover()
     {
-        image.color = transparentColor;
+        if (titleText != null)
+        {
+            targetScale = originalScale;
+            titleText.color = originalColor;
+        }
     }
 }
