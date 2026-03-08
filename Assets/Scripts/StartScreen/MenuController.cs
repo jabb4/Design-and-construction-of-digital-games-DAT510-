c05c3a3c56a03028cd5652c2cb901e8130327fa0
Jacob <87302129+jabb4@users.noreply.github.com>
2026-03-08 15:46:37 +0100

    Add intro video when starting new game

--- Assets/Scripts/StartScreen/MenuController.cs
+++ Assets/Scripts/StartScreen/MenuController.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MenuController : MonoBehaviour
 {
@@ -8,9 +9,15 @@ public class MenuController : MonoBehaviour
     [SerializeField] private GameObject newGameButton;
     [SerializeField] private GameObject quitGameButton;
 
+    [SerializeField] private GameObject videoPlayer;
+    [SerializeField] private GameObject screen;
+    [SerializeField] private AudioSource menuAudioSource;
+
     private Vector3 originalNewGameButtonLocalPosition;
     private Vector3 originalLoadGameButtonLocalPosition;
     private Vector3 originalQuitGameButtonLocalPosition;
+    private VideoPlayer cachedVideoPlayer;
+    private bool isStartingNewGame;
 
     private void Awake()
     {
@@ -18,35 +25,90 @@ public class MenuController : MonoBehaviour
         originalLoadGameButtonLocalPosition = loadGameButton.transform.localPosition;
         originalNewGameButtonLocalPosition = newGameButton.transform.localPosition;
         originalQuitGameButtonLocalPosition = quitGameButton.transform.localPosition;
-        
+
+        if (videoPlayer != null)
+        {
+            cachedVideoPlayer = videoPlayer.GetComponent<VideoPlayer>();
+        }
     }
-     
+
     private void OnEnable()
     {
+        screen.gameObject.SetActive(false);
+
+        if (cachedVideoPlayer != null)
+        {
+            cachedVideoPlayer.Stop();
+        }
+
+        isStartingNewGame = false;
+
         // Deactivate Load game button if there isn't any saved data
         if (!GameStateManager.Instance.gameSaveExists)
         {
             loadGameButton.SetActive(false);
             newGameButton.transform.localPosition = originalLoadGameButtonLocalPosition;
             quitGameButton.transform.localPosition = originalNewGameButtonLocalPosition;
-        } else {
+        }
+        else
+        {
             loadGameButton.SetActive(true);
             newGameButton.transform.localPosition = originalNewGameButtonLocalPosition;
             quitGameButton.transform.localPosition = originalQuitGameButtonLocalPosition;
         }
     }
 
+    private void OnDisable()
+    {
+        if (cachedVideoPlayer != null)
+        {
+            cachedVideoPlayer.loopPointReached -= OnNewGameVideoFinished;
+        }
+    }
+
     public void NewGame()
     {
-        GameStateManager.Instance.SetInitValues();
-        GameStateManager.Instance.SaveGameState();
-        SceneManager.LoadSceneAsync(1);
+        if (isStartingNewGame)
+        {
+            return;
+        }
+
+        isStartingNewGame = true;
+
+        if (menuAudioSource != null)
+        {
+            menuAudioSource.Stop();
+        }
+
+        if (cachedVideoPlayer == null)
+        {
+            CompleteNewGame();
+            return;
+        }
+
+        cachedVideoPlayer.loopPointReached -= OnNewGameVideoFinished;
+        cachedVideoPlayer.loopPointReached += OnNewGameVideoFinished;
+        cachedVideoPlayer.Stop();
+        cachedVideoPlayer.Play();
+        screen.gameObject.SetActive(true);
     }
 
     public void LoadGame()
     {
-        // Make sure we load the saved values
         GameStateManager.Instance.LoadGameState();
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void OnNewGameVideoFinished(VideoPlayer source)
+    {
+        source.loopPointReached -= OnNewGameVideoFinished;
+        CompleteNewGame();
+    }
+
+    private void CompleteNewGame()
+    {
+        GameStateManager.Instance.SetInitValues();
+        GameStateManager.Instance.SaveGameState();
         SceneManager.LoadSceneAsync(1);
     }
 
